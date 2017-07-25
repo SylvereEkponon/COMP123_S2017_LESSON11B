8b24bfa5c8ba83a440d7746a53d379fa89befba4
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-25 17:15:55 -0400

    Added the face enum

--- LINQDEMO/Face.cs
+++ LINQDEMO/Face.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/*
+ * Name: Sylvere Ekponon
+ * Date: July 25, 2017
+ * Description:this is the face enum
+ * Version:created the face enum
+ */
+
 namespace LINQDEMO
 {
     public enum Face
