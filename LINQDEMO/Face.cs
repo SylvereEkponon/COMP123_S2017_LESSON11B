3480db4f63a203163207b006de1420212e1048b1
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-25 17:15:08 -0400

    Added the suit enum

---
+++ LINQDEMO/Face.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQDEMO
+{
+    public enum Face
+    {
+        Ace =1,
+
+        One,
+
+        Two,
+       
+        Three,
+
+        Four,
+
+        Five,
+
+        Six,
+
+        Seven,
+
+        Eight,
+
+        Nine,
+
+        Ten,
+
+        Jack,
+
+        Queen,
+
+        King
+    }
+}
---
+++ LINQDEMO/Suit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Sylvere Ekponon
+ * Date: July 25, 2017
+ * Description:this is the suit enum
+ * Version:created the suit enum
+ */
+
+namespace LINQDEMO
+{
+    public enum Suit
+    {
+        Diamonds,
+        Clubs,
+        Hearts,
+        Spades
+    }
+}
